ea8cbf04d810397169160ca92f6ea98ed33e7406
okta ningrum <oktadug@gmail.com>
2021-12-08 16:32:37 +0700

    merubah program.cs

---
+++ NewRepoProject/NewRepoProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewRepoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Pemrograman Lanjut S1 Informatika");
+        }
+    }
+}
